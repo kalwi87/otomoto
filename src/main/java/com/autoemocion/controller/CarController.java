8d62685e88971eea9c5e870cc6672b4a978d79fa
lukasz <biel.lukasz@hotmail.com>
2017-10-16 13:47:51 +0200

    cors update

--- src/main/java/com/autoemocion/controller/CarController.java
+++ src/main/java/com/autoemocion/controller/CarController.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -21,7 +22,7 @@ public class CarController {
     public CarController(CarService carservice) {
         this.carservice = carservice;
     }
-
+    @CrossOrigin(origins = "http://autoemocion.pl/")
     @GetMapping
     public List<Car> getListOfCars() throws IOException {
         return carservice.findAll();
